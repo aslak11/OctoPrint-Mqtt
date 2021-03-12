3dc86d5f3f3975c67fa39260b2d651d6efc06818
Aslak Hellesund <aslakhellesund@gmail.com>
2021-03-12 14:36:34 +0100

    Initial commit

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ plugin_url = "https://github.com/aslak11/OctoPrint-Mqtt"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = []
+plugin_requires = ["paho-mqtt"]
 
 ### --------------------------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point
